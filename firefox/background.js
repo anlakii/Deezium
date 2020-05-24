4caf88d2dc8be40ec053008b22ed7ebfd248a308
lakiluki1 <lolwoot1234@gmail.com>
2020-05-24 21:34:43 +0200

    firefox: wip after the Deezer update

--- firefox/background.js
+++ firefox/background.js
@@ -1,3 +1,6 @@
+console.log("Loaded!");
+let API_KEY = "ZAIVAHCEISOHWAICUQUEXAEPICENGUAFAEZAIPHAELEEVAHPHUCUFONGUAPASUAY";
+
 function logListen(details) {
   let filter = browser.webRequest.filterResponseData(details.requestId);
   let decoder = new TextDecoder("utf-8");
@@ -10,6 +13,74 @@ function logListen(details) {
     json.results = false;
     str = JSON.stringify(json);
     filter.write(encoder.encode(str));
+    filter.disconnect();
+  };
+  return {};
+}
+
+function getAlbum(details) {
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = event => {
+    let str = decoder.decode(event.data, { stream: false });
+    console.log(str);
+    filter.write(encoder.encode(str));
+    filter.disconnect();
+  };
+  return {};
+}
+
+async function fetchFromMobile(method, postData) {
+  var sid = await getSid();
+  let requestUrl = 'https://api.deezer.com/1.0/gateway.php?method=' + method + '&input=3&output=2&api_key=' + API_KEY + '&sid=' + sid;
+  console.log(requestUrl);
+  let resp = await fetch(requestUrl, {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'omit',
+    body: JSON.stringify(postData)
+  });
+
+  return resp.json();
+}
+
+async function playlistGetSongs(details) {
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+  var posted = JSON.parse(decodeURIComponent(String.fromCharCode.apply(null,
+                                  new Uint8Array(details.requestBody.raw[0].bytes))));
+
+  var data = {'playlist_id': posted.playlist_id};
+  var resp = await fetchFromMobile('playlist.getSongs', data);
+  console.log(resp);
+
+  filter.ondata = event => {
+    let str = decoder.decode(event.data, { stream: false });
+    filter.write(encoder.encode(JSON.stringify(resp)));
+    filter.disconnect();
+  };
+  return {};
+}
+
+async function getListByAlbum(details) {
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+  var posted = JSON.parse(decodeURIComponent(String.fromCharCode.apply(null,
+                                  new Uint8Array(details.requestBody.raw[0].bytes))));
+  var album = posted.alb_id;
+  var data = {'alb_id': album};
+  var resp = await fetchFromMobile('song.getListByAlbum', data);
+
+  filter.ondata = event => {
+    let str = decoder.decode(event.data, { stream: false });
+    filter.write(encoder.encode(JSON.stringify(resp)));
     filter.disconnect();
   };
   return {};
@@ -28,8 +99,7 @@ function userData(details) {
     let aesEcb = new aesjs.ModeOfOperation.ecb(key);
 
     let encryptedBytes = aesjs.utils.hex.toBytes(json.results.PLAYER_TOKEN);
-    let decryptedBytes = aesEcb.decrypt(encryptedBytes);
-    let decryptedToken = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    let decryptedBytes = aesEcb.decrypt(encryptedBytes); let decryptedToken = aesjs.utils.utf8.fromBytes(decryptedBytes);
     let decryptedTokenJson = JSON.parse(decryptedToken.replace(/\0/g, ''))
 
     decryptedTokenJson.hq = true;
@@ -42,9 +112,10 @@ function userData(details) {
         decryptedTokenJson.audio_qualities[opt].push('lossless')
     }
 
+    console.log("TOKEN");
+    console.log(decryptedTokenJson);
     newToken = JSON.stringify(decryptedTokenJson);
     let tokenLength = newToken.length;
-    console.log(newToken.length)
 
     for(let i = 0; i < 16 - (tokenLength % 16); i++){
         newToken += '\0';
@@ -90,7 +161,50 @@ function userData(details) {
   return {};
 }
 
-console.log("Loaded!");
+
+async function getSid(){
+  var req = await fetch('https://www.deezer.com/ajax/gw-light.php?method=deezer.ping&api_version=1.0&api_token', {
+    credentials: 'omit'
+  });
+  var json = await req.json();
+  return json.results.SESSION;
+}
+
+
+browser.webRequest.onBeforeRequest.addListener(
+  getListByAlbum,
+  {
+    urls: [
+      "https://www.deezer.com/ajax/gw-light.php?method=song.getListByAlbum*"
+    ],
+    types: ["xmlhttprequest", "other", "script", "object_subrequest"]
+  },
+  ["blocking", "requestBody"]
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+  playlistGetSongs,
+  {
+    urls: [
+      "https://www.deezer.com/ajax/gw-light.php?method=playlist.getSongs*"
+    ],
+    types: ["xmlhttprequest", "other", "script", "object_subrequest"]
+  },
+  ["blocking", "requestBody"]
+);
+
+browser.webRequest.onBeforeRequest.addListener(
+  getAlbum,
+  {
+    urls: [
+      "https://www.deezer.com/us/album/*"
+    ],
+    types: ["xmlhttprequest", "other", "script", "object_subrequest"]
+  },
+  ["blocking", "requestBody"]
+);
+
+
 browser.webRequest.onBeforeRequest.addListener(
   userData,
   {
